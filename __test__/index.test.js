1b67d0cf6917a3b7a380b24ce154d76d58a90e81
jackie-gan <jackiegan405679096@gmail.com>
2020-09-01 20:53:47 +0800

    feat: update

--- __test__/index.test.js
+++ __test__/index.test.js
@@ -1,8 +1,8 @@
-import TinyPromise from '../src/index';
+import TestPromise from '../src';
 
 describe('test promise', () => {
   it('test case 1', () => {
-    const promise = new TinyPromise((resolve) => {
+    const promise = new TestPromise((resolve) => {
       setTimeout(() => {
         resolve(1);
       }, 1000);
@@ -14,6 +14,80 @@ describe('test promise', () => {
   })
 
   it('test case 2', () => {
-    TinyPromise.resolve(TinyPromise.resolve(1)).then((res) => { expect(res).toBe(1); })
+    TestPromise.resolve(TestPromise.resolve(1)).then((res) => { expect(res).toBe(1); })
+  })
+
+  it('test case 3', () => {
+    const promise = new TestPromise((resolve) => {
+      setTimeout(() => {
+        resolve(1);
+      }, 1000);
+    });
+
+    return promise.finally(() => {
+      expect(1).toBe(1);
+    });
+  })
+
+  it('test case 4', () => {
+    const promise = new TestPromise((resolve, reject) => {
+      setTimeout(() => {
+        reject(1);
+      }, 1000);
+    });
+
+    return promise.catch((res) => {
+      expect(res).toBe(1);
+    });
+  })
+
+  it('test case 5', () => {
+    return TestPromise.resolve(TestPromise.resolve(123)).then((res) => {
+      expect(res).toBe(123);
+    });
+  })
+
+  it('test case 6', () => {
+    return TestPromise.resolve(TestPromise.reject(123)).catch((res) => {
+      expect(res).toBe(123);
+    });
+  })
+
+  it('test case 7', () => {
+    return TestPromise.reject(123).catch((res) => {
+      expect(res).toBe(123);
+    });
+  })
+
+  it('test case 8', () => {
+    const promise1 = new TestPromise((resolve) => {
+      setTimeout(() => {
+        resolve(1);
+      }, 1000);
+    });
+
+    const promise2 = new TestPromise((resolve) => {
+      setTimeout(() => {
+        resolve(2);
+      }, 1500);
+    });
+
+    return TestPromise.all([promise1, promise2]).then((res) => {
+      expect(res).toEqual([1, 2]);
+    });
+  })
+
+  it('test case 9', () => {
+    const promise1 = new TestPromise((resolve) => {
+      setTimeout(resolve, 500, 'one');
+    });
+    
+    const promise2 = new TestPromise((resolve) => {
+      setTimeout(resolve, 100, 'two');
+    });
+    
+    return TestPromise.race([promise1, promise2]).then((res) => {
+      expect(res).toEqual('two');
+    });
   })
 });
--- src/index.js
+++ src/index.js
@@ -1,135 +1,114 @@
+/**
+ * Promise/A+规范：https://promisesaplus.com/
+ */
+
+// 三种Promise状态
 const PENDING = 'PENDING';
-const FULLFILLED = 'FULLFILLED';
+const FULFILLED = 'FULFILLED';
 const REJECTED = 'REJECTED';
 
-class TinyPromise {
+class TestPromise {
   constructor(handler) {
-    if (typeof handler !== 'function') {
-      throw new Error('handler should be function');
-    }
-
     this._status = PENDING;
     this._value = undefined;
-
-    this._fullFillQueue = [];
+    this._fulfillQueue = [];
     this._rejectQueue = [];
 
+    function _resolve(val) {
+      // 当构造函数中多次调用resolve时，只认可第一次调用
+      if (this._status !== PENDING) return;
+
+      this._status = FULFILLED;
+      this._value = val;
+
+      let currentFunc;
+
+      while (currentFunc = this._fulfillQueue.shift()) {
+        currentFunc(this._value);
+      }
+    }
+    
+    function _reject(err) {
+      // 当构造函数中多次调用reject时，只认可第一次调用
+      if (this._status !== PENDING) return;
+
+      this._status = REJECTED;
+      this._value = err;
+
+      let currentFunc;
+
+      while (currentFunc = this._rejectQueue.shift()) {
+        currentFunc(this._value);
+      }
+    }
+
     try {
-      handler(this._resolve.bind(this), this._reject.bind(this));
-    } catch (error) {
-      this._reject(error);
+      handler(_resolve.bind(this), _reject.bind(this));
+    } catch (e) {
+      this._reject(e);
     }
   }
 
-  _resolve(value) {
-    if (this._status !== PENDING) return;
-
-    this._status = FULLFILLED;
-    this._value = value;
-
-    let callback;
-
-    while (callback = this._fullFillQueue.shift()) {
-      callback(value);
-    }
-  }
-
-  _reject(value) {
-    if (this._status !== PENDING) return;
-
-    this._status = REJECTED;
-    this._value = value;
-
-    let callback;
-
-    while (callback = this._rejectQueue.shift()) {
-      callback(value);
-    }
-  }
-
-  static resolve(value) {
-    if (value instanceof TinyPromise) return value;
-    return new TinyPromise((resolve) => {
-      resolve(value);
-    });
-  }
-
-  static reject(value) {
-    return new TinyPromise((resolve, reject) => {
-      reject(value);
-    });
-  }
-
-  static all(promises) {
-    let count = 0;
-    const result = [];
-
-    return new TinyPromise((resolve, reject) => {
-      promises.forEach(promise => {
-        TinyPromise.resolve(promise).then((value) => {
-          count++;
-          result.push(value);
-          if(count === promise.length) {
-            resolve(result)
-          }
-        }, (err) => {
-          reject(err);
-        });
-        promise.finally(finishCallBack);
-      });
-    });
-  }
-
-  then(onFullfilled, onRejected) {
+  then(onFulfilled, onRejected) {
     const currentStatus = this._status;
     const currentValue = this._value;
-    
-    return new TinyPromise((onFullfilledNext, onRejectedNext) => {
+    // then和catch必须返回一个Promise
+    return new TestPromise((resolveNext, rejectNext) => {
       const resolveCall = (value) => {
         try {
-          if (typeof onFullfilled !== 'function') {
-            // 值穿透
-            onFullfilledNext(value);
+          if (typeof onFulfilled !== 'function') {
+            // 当 onFulfilled 不是function时，则忽略
+            resolveNext(value);
           } else {
-            const ret = onFullfilled(value);
-            if (ret instanceof TinyPromise) {
-              ret.then(onFullfilledNext, onRejectedNext);
+            // 当 onFulfilled 是function时，则执行
+            const ret = onFulfilled(value);
+            if (ret instanceof TestPromise) {
+              // 返回值是Promise，则透传Promise的结果
+              ret.then(resolveNext, rejectNext);
             } else {
-              onFullfilledNext(ret);
+              // 范围值不是Promise，则正常执行
+              resolveNext(ret);
             }
           }
-        } catch (error) {
-          onRejectedNext(error);
+        } catch (e) {
+          rejectNext(e);
         }
       };
 
-      const rejectCall = (value) => {
+      const rejectCall = (err) => {
         try {
           if (typeof onRejected !== 'function') {
-            // 值穿透
-            onRejectedNext(value);
+            // 当 onRejected 不是function时，则忽略
+            rejectNext(err);
           } else {
-            const ret = onRejected(value);
-            if (ret instanceof TinyPromise) {
-              ret.then(onFullfilledNext, onRejectedNext);
+            // 当 onRejected 是function时，则执行
+            const ret = onRejected(err);
+            if (ret instanceof TestPromise) {
+              // 返回值是Promise，则透传Promise的结果
+              ret.then(resolveNext, rejectNext);
             } else {
-              onFullfilledNext(ret);
+              // 范围值不是Promise，则正常执行
+              resolveNext(ret);
             }
           }
-        } catch (error) {
-          onRejectedNext(error);
+        } catch (e) {
+          rejectNext(e);
         }
       };
 
+      // 核心：需要根据当前Promise的状态，作不同的处理
       switch (currentStatus) {
         case PENDING:
-          this._fullFillQueue.push(resolveCall);
+          // 当Promise状态为PENDING，表示需要等待异步操作完成后，才执行resolve 或 reject callback
+          this._fulfillQueue.push(resolveCall);
           this._rejectQueue.push(rejectCall);
           break;
-        case FULLFILLED:
+        case FULFILLED:
+          // 当Promise状态为FULFILLED，则立即执行then中的resolve callback
           resolveCall(currentValue);
           break;
         case REJECTED:
+          // 当Promise状态为REJECTED，则立即执行catch中的reject callback
           rejectCall(currentValue);
           break;
       }
@@ -137,10 +116,14 @@ class TinyPromise {
   }
 
   catch(onRejected) {
+    // catch只借用reject部分
+    // 此处调用then时，除了返回promise外，还将onReject回调添加到_rejectQueue队列中
     return this.then(undefined, onRejected);
   }
 
   finally(callback) {
+    // 返回Promise
+    // 无论当前Promise是什么状态，都会执行finally的callback
     return this.then((value) => {
       callback();
       return value;
@@ -149,6 +132,62 @@ class TinyPromise {
       throw error;
     });
   }
+
+  /**
+   * 如果入参value为Promise，则直接返回Promise
+   * 否则，返回包装value后的Promise 
+   */
+  static resolve(value) {
+    if (value instanceof TestPromise) {
+      return value;
+    } else {
+      return new TestPromise((resolve) => {
+        resolve(value);
+      });
+    }
+  }
+
+  // 返回一个带拒绝原因的Promise
+  // 这里不会对入参进行类型判断
+  static reject(err) {
+    return new TestPromise((resolve, reject) => {
+      reject(err);
+    });
+  }
+
+  // promises中所有的任务执行完成，则resolve
+  // 中间有报错，则reject
+  static all(promises) {
+    let count = 0;
+    const promiseResult = [];
+
+    return new TestPromise((resolve, reject) => {
+      for (const promise of promises) {
+        promise.then((value) => {
+          count += 1;
+          promiseResult.push(value);
+          if (count === promises.length) {
+            resolve(promiseResult);
+          }
+        }, (err) => {
+          reject(err);
+        });
+      }
+    });
+  }
+
+  // 返回一个 promise，一旦迭代器中的某个promise解决或拒绝，返回的 promise就会解决或拒绝
+  static race(promises) {
+    return new TestPromise((resolve, reject) => {
+      for (const promise of promises) {
+        promise.then((value) => {
+          resolve(value);
+        }, (err) => {
+          reject(err);
+        });
+      }
+    });
+  }
 }
 
-export default TinyPromise;
+export default TestPromise;
